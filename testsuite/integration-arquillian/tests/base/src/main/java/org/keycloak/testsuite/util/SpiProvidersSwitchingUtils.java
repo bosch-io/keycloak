6bcd20f5052516dc9093d7e6aba61a9a3b4b7133
Joerg Matysiak <joerg.matysiak@bosch.io>
2021-10-15 11:01:35 +0200

    Ensure previously configured default provider is restored after test with special provider settings

--- testsuite/integration-arquillian/tests/base/src/main/java/org/keycloak/testsuite/util/SpiProvidersSwitchingUtils.java
+++ testsuite/integration-arquillian/tests/base/src/main/java/org/keycloak/testsuite/util/SpiProvidersSwitchingUtils.java
@@ -1,25 +1,32 @@
 package org.keycloak.testsuite.util;
 
+import org.jboss.logging.Logger;
 import org.keycloak.testsuite.arquillian.AuthServerTestEnricher;
 import org.keycloak.testsuite.arquillian.ContainerInfo;
 import org.keycloak.testsuite.arquillian.SuiteContext;
 import org.keycloak.testsuite.arquillian.annotation.SetDefaultProvider;
 import org.keycloak.testsuite.arquillian.containers.KeycloakQuarkusServerDeployableContainer;
 import org.wildfly.extras.creaper.core.online.CliException;
+import org.wildfly.extras.creaper.core.online.ModelNodeResult;
 import org.wildfly.extras.creaper.core.online.OnlineManagementClient;
 
 import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class SpiProvidersSwitchingUtils {
 
     private static final String SUBSYSTEM_KEYCLOAK_SERVER_SPI = "/subsystem=keycloak-server/spi=";
     private static final String KEYCLOAKX_ARG_SPI_PREFIX = "--spi-";
-
+    private static final Map<String, String> originalSettingsBackup = new ConcurrentHashMap<>();
+    protected static final Logger log = Logger.getLogger(SpiProvidersSwitchingUtils.class);
+    
     private SpiProvidersSwitchingUtils() {}
 
     public static void addProviderDefaultValue(SuiteContext suiteContext, SetDefaultProvider annotation) throws IOException, CliException {
         ContainerInfo authServerInfo = suiteContext.getAuthServerInfo();
 
+        log.infof("Setting default provider for {} to {}", annotation.spi(), annotation.providerId());
         if (authServerInfo.isUndertow()) {
             System.setProperty("keycloak." + annotation.spi() + ".provider", annotation.providerId());
         } else if (authServerInfo.isQuarkus()) {
@@ -29,6 +36,7 @@ public class SpiProvidersSwitchingUtils {
             OnlineManagementClient client = AuthServerTestEnricher.getManagementClient();
 
             if (annotation.onlyUpdateDefault()) {
+                backupOriginalSpiDefaultProvider(client, annotation.spi());
                 client.execute(SUBSYSTEM_KEYCLOAK_SERVER_SPI + annotation.spi() + ":write-attribute(name=default-provider, value=" + annotation.providerId() + ")");
             } else {
                 client.execute(SUBSYSTEM_KEYCLOAK_SERVER_SPI + annotation.spi() + "/:add(default-provider=\"" + annotation.providerId() + "\")");
@@ -38,9 +46,43 @@ public class SpiProvidersSwitchingUtils {
         }
     }
 
+    private static void backupOriginalSpiDefaultProvider(OnlineManagementClient client, String spi) throws CliException, IOException {
+        ModelNodeResult readResult = client.execute(SUBSYSTEM_KEYCLOAK_SERVER_SPI + spi + ":read-attribute(name=default-provider)");
+        if (readResult.isSuccess()) {
+            originalSettingsBackup.put(spi, readResult.get("result").asString());
+        } 
+    }
+
+    public static void resetProvider(SuiteContext suiteContext, SetDefaultProvider annotation) throws IOException, CliException {
+        ContainerInfo authServerInfo = suiteContext.getAuthServerInfo();
+
+        log.infof("Resetting default provider for {}", annotation.spi());
+        if (authServerInfo.isUndertow()) {
+            System.clearProperty("keycloak." + annotation.spi() + ".provider");
+        } else if (authServerInfo.isQuarkus()) {
+            KeycloakQuarkusServerDeployableContainer container = (KeycloakQuarkusServerDeployableContainer) authServerInfo.getArquillianContainer().getDeployableContainer();
+            container.resetConfiguration(true);
+        } else {
+            OnlineManagementClient client = AuthServerTestEnricher.getManagementClient();
+            if (annotation.onlyUpdateDefault()) {
+                String originalValue = originalSettingsBackup.get(annotation.spi());
+                log.debugf("Resetting default provider for {} to ", annotation.spi(), originalValue != null ? originalValue : "undefined");
+                if (originalValue != null) {
+                    client.execute(SUBSYSTEM_KEYCLOAK_SERVER_SPI + annotation.spi() + "/:write-attribute(name=default-provider,value=" + originalValue + ")");
+                } else {
+                    client.execute(SUBSYSTEM_KEYCLOAK_SERVER_SPI + annotation.spi() + "/:undefine-attribute(name=default-provider)");
+                }
+            } else {
+                client.execute(SUBSYSTEM_KEYCLOAK_SERVER_SPI + annotation.spi() + "/:remove");
+            }
+            client.close();
+        }
+    }
+    
     public static void removeProvider(SuiteContext suiteContext, SetDefaultProvider annotation) throws IOException, CliException {
         ContainerInfo authServerInfo = suiteContext.getAuthServerInfo();
 
+        log.infof("Removing default provider setting for {}", annotation.spi());
         if (authServerInfo.isUndertow()) {
             System.clearProperty("keycloak." + annotation.spi() + ".provider");
         } else if (authServerInfo.isQuarkus()) {
