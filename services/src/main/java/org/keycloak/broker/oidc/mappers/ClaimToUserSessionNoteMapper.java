8d52bd731051990f8a9365ecc84605c2dfc0b276
danielFesenmeyer <daniel.fesenmeyer@bosch.io>
2023-03-16 10:10:19 +0100

    Suggested review changes:
    - AbstractClaimToUserSessionNoteMapper is not needed, merge content of AbstractClaimToUserSessionNoteMapper into ClaimToUserSessionNoteMapper
    - ClaimToUserSessionNoteMapper: constants should be private, cause they are used nowhere else now
    - ClaimToUserSessionNoteMapper: configProperties field is a constant and should therefore follow the convention: CONFIG_PROPERTIES
    - ClaimToUserSessionNoteMapper: implement method supportsSyncMode, otherwise syncMode "FORCE" may not be configurable via UI
    - ClaimToUserSessionNoteMapper: the configured claim value (e.g. '.*' for a regex) was written to the user session note, changed it to the actual value (TODO: support values other than string, at least arrays)
    - KeycloakTestingClient: As there are no real changes to that file (besides import cleanup), I would not change it
    - AbstractUserSessionNoteMapperTest is not needed, merge content of AbstractUserSessionNoteMapperTest into OidcClaimToUserSessionNoteMapperTest
    - OidcClaimToUserSessionNoteMapperTest: consumerClientRep and constants should be private, cause they are used nowhere else now
    - OidcClaimToUserSessionNoteMapperTest#createUserSessionNoteIdpMapper (former #createMapperInIdp): use variable name "mapper" instead of incorrect "claimToRoleMapper"
    - OidcClaimToUserSessionNoteMapperTest#setup: createUserInProviderRealm: email is not necessary, and use constants instead of firstName and lastName
    - OidcClaimToUserSessionNoteMapperTest: add additional tests for import/force behavior

--- services/src/main/java/org/keycloak/broker/oidc/mappers/ClaimToUserSessionNoteMapper.java
+++ services/src/main/java/org/keycloak/broker/oidc/mappers/ClaimToUserSessionNoteMapper.java
@@ -1,58 +1,132 @@
 package org.keycloak.broker.oidc.mappers;
 
+import static org.keycloak.utils.RegexUtils.valueMatchesRegex;
+
+import org.jboss.logging.Logger;
 import org.keycloak.broker.oidc.KeycloakOIDCIdentityProviderFactory;
 import org.keycloak.broker.oidc.OIDCIdentityProviderFactory;
+import org.keycloak.broker.provider.BrokeredIdentityContext;
+import org.keycloak.models.IdentityProviderMapperModel;
+import org.keycloak.models.IdentityProviderSyncMode;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.RealmModel;
+import org.keycloak.models.UserModel;
 import org.keycloak.provider.ProviderConfigProperty;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
-public class ClaimToUserSessionNoteMapper extends AbstractClaimToUserSessionNoteMapper {
+public class ClaimToUserSessionNoteMapper extends AbstractClaimMapper {
 
-    static final String ARE_CLAIM_VALUES_REGEX_PROPERTY_NAME = "are.claim.values.regex";
-    static final String[] COMPATIBLE_PROVIDERS = {KeycloakOIDCIdentityProviderFactory.PROVIDER_ID,
+    private static final Logger LOG = Logger.getLogger(ClaimToUserSessionNoteMapper.class);
+
+    private static final String CLAIM_PROPERTY_NAME = "claims";
+    private static final String ARE_CLAIM_VALUES_REGEX_PROPERTY_NAME = "are.claim.values.regex";
+    private static final String[] COMPATIBLE_PROVIDERS = {KeycloakOIDCIdentityProviderFactory.PROVIDER_ID,
             OIDCIdentityProviderFactory.PROVIDER_ID};
 
-    private static final List<ProviderConfigProperty> configProperties = new ArrayList<>();
+    private static final List<ProviderConfigProperty> CONFIG_PROPERTIES = new ArrayList<>();
+
+    private static final Set<IdentityProviderSyncMode> IDENTITY_PROVIDER_SYNC_MODES =
+            new HashSet<>(Arrays.asList(IdentityProviderSyncMode.values()));
 
     static {
         ProviderConfigProperty claimsProperty = new ProviderConfigProperty();
         claimsProperty.setName(CLAIM_PROPERTY_NAME);
         claimsProperty.setLabel("Claims");
-        claimsProperty.setHelpText("Name and value of the claims to search for in token. You can reference nested claims using a '.', i.e. 'address.locality'. To use dot (.) literally, escape it with backslash (\\.)");
+        claimsProperty.setHelpText(
+                "Name and value of the claims to search for in token. You can reference nested claims using a '.', i.e. 'address.locality'. To use dot (.) literally, escape it with backslash (\\.)");
         claimsProperty.setType(ProviderConfigProperty.MAP_TYPE);
-        configProperties.add(claimsProperty);
+        CONFIG_PROPERTIES.add(claimsProperty);
         ProviderConfigProperty isClaimValueRegexProperty = new ProviderConfigProperty();
         isClaimValueRegexProperty.setName(ARE_CLAIM_VALUES_REGEX_PROPERTY_NAME);
         isClaimValueRegexProperty.setLabel("Regex Claim Values");
         isClaimValueRegexProperty.setHelpText("If enabled, claim values are interpreted as regular expressions.");
         isClaimValueRegexProperty.setType(ProviderConfigProperty.BOOLEAN_TYPE);
-        configProperties.add(isClaimValueRegexProperty);
+        CONFIG_PROPERTIES.add(isClaimValueRegexProperty);
     }
 
     public static final String PROVIDER_ID = "oidc-user-session-note-idp-mapper";
 
-    @Override public String[] getCompatibleProviders() {
+    @Override
+    public String[] getCompatibleProviders() {
         return COMPATIBLE_PROVIDERS;
     }
 
-    @Override public String getDisplayCategory() {
+    @Override
+    public String getDisplayCategory() {
         return "User Session";
     }
 
-    @Override public String getDisplayType() {
+    @Override
+    public String getDisplayType() {
         return "User Session Note Mapper";
     }
 
-    @Override public String getHelpText() {
+    @Override
+    public String getHelpText() {
         return "Add every matching claim to the user session note.";
     }
 
-    @Override public List<ProviderConfigProperty> getConfigProperties() {
-        return configProperties;
+    @Override
+    public List<ProviderConfigProperty> getConfigProperties() {
+        return CONFIG_PROPERTIES;
     }
 
-    @Override public String getId() {
+    @Override
+    public String getId() {
         return PROVIDER_ID;
     }
+
+    @Override
+    public boolean supportsSyncMode(IdentityProviderSyncMode syncMode) {
+        return IDENTITY_PROVIDER_SYNC_MODES.contains(syncMode);
+    }
+
+    @Override
+    public void importNewUser(KeycloakSession session, RealmModel realm, UserModel user,
+            IdentityProviderMapperModel mapperModel, BrokeredIdentityContext context) {
+        addClaimToSessionNote(mapperModel, context);
+    }
+
+    @Override
+    public void updateBrokeredUser(KeycloakSession session, RealmModel realm, UserModel user,
+            IdentityProviderMapperModel mapperModel, BrokeredIdentityContext context) {
+        addClaimToSessionNote(mapperModel, context);
+    }
+
+    private void addClaimToSessionNote(IdentityProviderMapperModel mapperModel, BrokeredIdentityContext context) {
+        Map<String, String> claims = mapperModel.getConfigMap(CLAIM_PROPERTY_NAME);
+        boolean areClaimValuesRegex =
+                Boolean.parseBoolean(mapperModel.getConfig().get(ARE_CLAIM_VALUES_REGEX_PROPERTY_NAME));
+
+        for (Map.Entry<String, String> claim : claims.entrySet()) {
+            Object valueObj = getClaimValue(context, claim.getKey());
+
+            if (valueObj == null) {
+                break;
+            }
+
+            if (!(valueObj instanceof String)) {
+                LOG.warnf(
+                        "Claim '%s' does not contain a string value for user with brokerUserId '%s'. Actual value is of type '%s': %s",
+                        claim.getKey(),
+                        context.getBrokerUserId(), valueObj.getClass(), valueObj);
+                break;
+            }
+
+            String value = (String) valueObj;
+
+            boolean claimValuesMatch = areClaimValuesRegex ? valueMatchesRegex(claim.getValue(), value)
+                    : valueEquals(claim.getValue(), value);
+            if (claimValuesMatch) {
+                context.getAuthenticationSession().setUserSessionNote(claim.getKey(), value);
+            }
+        }
+    }
+
 }
